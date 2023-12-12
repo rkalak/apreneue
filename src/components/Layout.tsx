7939b9966ccae611253db0d35cb9eda573931669
Rahul Kalakuntla <rkalak01@gmail.com>
2023-12-11 21:46:46 -0600

    color change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,12 +14,12 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         {/* Add the link tag for the favicon in the head section */}
         <link rel="icon" href="/images/aprefavicon.png" />
       </Head>      
-      <header style={{ textAlign: 'center', padding: '10px', backgroundColor: 'rgba(0, 0, 0, 0)', color: '#fff', fontFamily: 'Helvetica Neue', fontWeight: 'bold', margin: 0, letterSpacing: '10px' }}>
+      <header style={{ textAlign: 'center', padding: '10px', backgroundColor: 'rgba(0, 0, 0, 0)', color: '#000', fontFamily: 'Helvetica Neue', fontWeight: 'bold', margin: 0, letterSpacing: '10px' }}>
         <h1>apreneue</h1>
         {/* Add navigation links here */}
       </header>
       <main style={{ flex: '1' }}>{children}</main>
-      <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: 'rgba(0, 0, 0, 0)', color: '#fff', fontFamily: 'Helvetica Neue', marginTop: 'auto', margin: 0 }}>&copy; 2023 apreneue</footer>
+      <footer style={{ textAlign: 'center', padding: '10px', backgroundColor: 'rgba(0, 0, 0, 0)', color: '#000', fontFamily: 'Helvetica Neue', marginTop: 'auto', margin: 0 }}>&copy; 2023 apreneue</footer>
     </div>
   );
 };
